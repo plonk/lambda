24a9f2f98d06e5822062c7dc85aacb1fbb8e2221
Yoteichi <plonk@piano.email.ne.jp>
2014-02-09 02:13:25 +0900

    expand

--- main.rb
+++ main.rb
@@ -2,6 +2,29 @@
 require './lambda.tab.rb'
 require  './lexer.rb'
 
+def expand(node)
+  case node
+  when Abst
+    varlist = node[0]
+    raise unless varlist.is_a? VList
+    case varlist.size
+    when 0
+      raise 'varlist empty'
+    when 1
+      node
+    else
+      copy = node.dup
+      copy[0] = copy[0].dup
+      vname = copy[0].shift
+      Abst.new.replace [VList.new.replace([vname]), expand(copy)]
+    end
+  when String
+    node
+  else
+    node.class.new.replace node.map{|x| expand(x)} 
+  end
+end
+
 def read_eval_print_loop
   while true
     print "REPL> "
@@ -11,6 +34,8 @@ def read_eval_print_loop
     cp =  LambdaParser.new(lexer)
     root = cp.parse
     p root
+puts "カリー化します:"
+    p expand(root)
     puts
   end
 end
